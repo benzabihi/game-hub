40f209d68a9873119cd3651651c211fa26bc5f80
Ben Zabihi <beza6579@colorado.edu>
2024-04-10 23:35:32 -0600

    built game cards and resized for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">aside</GridItem>
+        <GridItem area="aside"></GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
